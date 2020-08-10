9e901093bd7bf506ba59107f400afa368fe46817
aditya gupta <adityasgupta309@gmail.com>
2020-08-10 16:24:49 +0530

    client-server side

--- vue-router-auth/server/app.js
+++ vue-router-auth/server/app.js
@@ -5,7 +5,6 @@ const config = require('./config')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const bodyParser = require('body-parser')
-
 const db = new DB('sqlitedb')
 const app = express()
 const router = express.Router()
